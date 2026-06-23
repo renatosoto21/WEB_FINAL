d2de0212478c1104246c1993296fe5460280f726
renatosoto21 <rsotoco@usm.cl>
2026-06-23 16:54:18 -0400

    ahora si

--- mysite/settings.py
+++ mysite/settings.py
@@ -101,7 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'America/Santiago'
 
